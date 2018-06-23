d8e9583ab43b37b7a1786eddecbb6f15b406a75d
XingMeansOK <771267736@qq.com>
2018-06-23 09:18:02 +0800

    init

---
+++ src/components/Picker/index.js
@@ -0,0 +1,14 @@
+import PickerComponent from './tpl.vue'
+
+const Picker = {
+    install: function( Vue, options ) {
+
+        Vue.prototype.$picker = function (options) {
+            
+        }
+
+    },
+
+}
+
+export default Picker
